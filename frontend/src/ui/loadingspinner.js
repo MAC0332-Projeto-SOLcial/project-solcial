a3a6d74845afdafa58bafaff0dd7a97c6bf84584
Less <155570422+lesszin@users.noreply.github.com>
2025-11-14 16:26:04 -0300

    Refactor Solar Form page (#5)
    * refactor solar form page
    
    * fix: input.js
    
    * fix: use only the necessary libs

---
+++ frontend/src/ui/loadingspinner.js
@@ -0,0 +1,7 @@
+const LoadingSpinner = ({ className = 'h-12 w-12' }) => {
+  return (
+    <div className={`animate-spin rounded-full border-b-2 border-green-600 ${className}`}></div>
+  );
+};
+
+export { LoadingSpinner };
